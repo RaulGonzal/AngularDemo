749f5568b9bf71bba872015e6109655014a1e144
RaulGonzal <Raulgonzalc@yahoo.com>
2024-08-23 19:43:35 -0400

    configuring custome component

---
+++ src/app/header.component.ts
@@ -0,0 +1,17 @@
+import { Component } from "@angular/core";
+@Component({
+    // selects elements that should be replaced by our component
+    // two part name as to not overwrite built in names
+    selector: 'app-header', 
+
+    // marks component as standalone component v.s module if set to false
+    // recomended to work with standalone components
+    standalone: true, 
+
+    // html
+    templateUrl: './header.component.html'
+    
+    // css
+})
+
+export class HeaderComponent {}
